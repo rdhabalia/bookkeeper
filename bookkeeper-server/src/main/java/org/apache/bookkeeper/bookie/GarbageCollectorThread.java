1bc89b906297a1372123cad69bd8689085fef333
Sahaya Andrews <sandrews@yahoo-inc.com>
2015-04-14 12:48:49 -0700

    [UPSTREAM] BOOKKEEPER-840 CMS-375 - Deadlock on flushLock on compaction

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/GarbageCollectorThread.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/GarbageCollectorThread.java
@@ -200,18 +200,18 @@ public class GarbageCollectorThread extends SafeRunnable {
                     LOG.debug("Skipping entry log flushing, as there is no offset!");
                     return;
                 }
-                synchronized (flushLock) {
-                    EntryLocation lastOffset = offsets.get(offsets.size()-1);
-                    long lastOffsetLogId = EntryLogger.logIdForOffset(lastOffset.location);
-                    while (lastOffsetLogId < entryLogger.getLeastUnflushedLogId() && running) {
+                EntryLocation lastOffset = offsets.get(offsets.size()-1);
+                long lastOffsetLogId = EntryLogger.logIdForOffset(lastOffset.location);
+                while (lastOffsetLogId < entryLogger.getLeastUnflushedLogId() && running) {
+                    synchronized (flushLock) {
                         flushLock.wait(1000);
+                    }
 
-                        lastOffset = offsets.get(offsets.size()-1);
-                        lastOffsetLogId = EntryLogger.logIdForOffset(lastOffset.location);
-                    }
-                    if (lastOffsetLogId >= entryLogger.getLeastUnflushedLogId() && !running) {
-                        throw new IOException("Shutdown before flushed");
-                    }
+                    lastOffset = offsets.get(offsets.size()-1);
+                    lastOffsetLogId = EntryLogger.logIdForOffset(lastOffset.location);
+                }
+                if (lastOffsetLogId >= entryLogger.getLeastUnflushedLogId() && !running) {
+                    throw new IOException("Shutdown before flushed");
                 }
             } catch (InterruptedException ie) {
                 Thread.currentThread().interrupt();
