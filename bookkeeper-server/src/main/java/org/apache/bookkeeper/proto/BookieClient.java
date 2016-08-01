957916413929b88e1ba1d70e579f669cdaed41e9
Sangmin Lee <sangmin@yahoo-inc.com>
2016-08-01 13:26:29 -0700

    CMS-1821: Bookie client keeps trying to connect to bookies that were taken OOR, and fills up the logs

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieClient.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieClient.java
@@ -329,7 +329,7 @@ public class BookieClient implements PerChannelBookieClientFactory {
     public void monitorPendingOperations() {
         for (PerChannelBookieClientPool clientPool : channels.values()) {
             for (int idx = 0; idx < numConnectionsPerBookie; idx++) {
-                clientPool.obtain(new GenericCallback<PerChannelBookieClient>() {
+                clientPool.get(new GenericCallback<PerChannelBookieClient>() {
 
                     @Override
                     public void operationComplete(int rc, PerChannelBookieClient result) {
